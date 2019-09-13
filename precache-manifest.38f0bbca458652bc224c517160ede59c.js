1f300814e2f69c6f317b666c55cc4eecd3a06ce0
Tawish Naqvi <tanaqvi@microsoft.com>
2019-09-13 10:33:47 -0700

    Updates

---
+++ precache-manifest.38f0bbca458652bc224c517160ede59c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46bb3b7e795239ca980bf5dbda3ca851",
+    "url": "/tawish1310/directorsCut/website/index.html"
+  },
+  {
+    "revision": "ad8d1eb155a2d25931b2",
+    "url": "/tawish1310/directorsCut/website/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "c290e6b4140c0eac94c2",
+    "url": "/tawish1310/directorsCut/website/static/js/2.de60ffef.chunk.js"
+  },
+  {
+    "revision": "ad8d1eb155a2d25931b2",
+    "url": "/tawish1310/directorsCut/website/static/js/main.b11edfb8.chunk.js"
+  },
+  {
+    "revision": "fe754e0c776c99d7fc7e",
+    "url": "/tawish1310/directorsCut/website/static/js/runtime~main.73eb05e8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tawish1310/directorsCut/website/static/media/logo.5d5d9eef.svg"
+  }
+]);
